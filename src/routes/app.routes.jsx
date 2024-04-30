c1b89f0428691e2b0895cf8ebf74e25c04618b54
Jocelito Junior <jocelito64@hotmail.com>
2024-04-29 22:31:32 -0300

    :sparkles: feat: Adjusting Routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route} from 'react-router-dom';
 
-//import { Home } from '../pages/Home'
-//import { DishDetails } from '../pages/DishDetails'
+import { Home } from '../pages/Home'
+import { DishDetails } from '../pages/DishDetails'
 import { NewDish } from '../pages/NewDish'
-//import { SignIn } from '../pages/SignIn'
-//import { SignUp } from '../pages/SignUp'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<NewDish />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/details/:id" element={<DishDetails />} />
+      <Route path="/new" element={<NewDish />} />
     </Routes>
   );
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route} from 'react-router-dom';
 
-import { Home } from '../pages/Home'
+import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp'
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
     </Routes>
   );
 }
